7826b37805a7d91f55d5883d08562fa4c3617371
Sam <smumo4751@gmail.com>
2024-11-06 11:04:42 +0300

    Update styles and templates for store page layout

--- quickmat_ecommerce/urls.py
+++ quickmat_ecommerce/urls.py
@@ -1,23 +1,25 @@
-"""
-URL configuration for quickmat_ecommerce project.
+# from django.contrib import admin
+# from django.urls import path, include
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('store.urls')),
+# ]
+
+from django.contrib import admin 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
+from store import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls')),
+    path('products/', include('store.urls')),
+    path('', views.product_list, name='home'), 
+    # path('media/<path:path>', views.serve_media, name='media'),  
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/models.py
+++ store/models.py
@@ -1,16 +1,3 @@
-# from django.db import models
-
-# class Product(models.Model):
-#     name = models.CharField(max_length=255)
-#     description = models.TextField()
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     image = models.ImageField(upload_to='products/')
-
-#     def __str__(self):
-#         return self.name
-
-
-# models.py
 from django.db import models
 
 class Category(models.Model):
@@ -19,10 +6,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)  # Assuming 1 is a valid category ID
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)  # One category per product
+    # image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+
+
     def __str__(self):
         return self.name
